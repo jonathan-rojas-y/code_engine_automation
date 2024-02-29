a39ef662f8e7329914736557d2b0f63de4de58e0
flamer <jonathanry98@gmail.com>
2024-02-29 15:02:07 -0500

    Test

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from upload_cos import upload_file, list_objects_buckets
 from create_files import generate_file
 import os
+from datetime import datetime
 
 days_diferential = os.environ.get("DAYS_DIFERENTIAL")
 bucket_name = os.environ.get("BUCKET_NAME")
 
+hora_actual = datetime.now().time()
 
-# for i in range(1, 11):
-#     filename = generate_file("automatically_created_file")
-#     upload_file(BUCKET_NAME, filename, filename)
-
-list_objects_buckets(bucket_name, float(days_diferential))
+if hora_actual < datetime.strptime('12:00:00', '%H:%M:%S').time():
+    for i in range(1, 11):
+        filename = generate_file("automatically_created_file")
+        upload_file(bucket_name, filename, filename)
+else:
+    list_objects_buckets(bucket_name, float(days_diferential))
